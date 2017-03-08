56411f9bdb843cbeccf763775766bf04fc8a110e
Сергей Дударев <dudarev-s@mail.ru>
2017-03-08 18:46:54 +0300

    'working_version'

--- homework_2.1.py
+++ homework_2.1.py
@@ -1,81 +1,40 @@
-cook_book = {
-    'Яичница': [
-        {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
-        {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
-    ],
-    'Стейк': [
-        {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
-        {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
-        {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'}
-    ],
-    'Салат': [
-        {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
-        {'ingridient_name': 'оругцы', 'quantity': 100, 'measure': 'гр.'},
-        {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
-        {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
-    ]
-}
+def recipes():
+    cook_book ={}
+    with open('new.txt', encoding= 'utf8') as f:
+        recipe = []
+        for entry in f:
+            entry = entry.strip()
+            if entry == '':
+                cook_book.update(pars_recipe(recipe))
+                recipe = []
+            else:
+                recipe.append(entry)
+        cook_book.update(pars_recipe(recipe))
+    return cook_book
 
-dishes = ['Яичница', 'Стейк', 'Салат']
+def pars_recipe(recipe):
+    cook = {}
+    dish = recipe[0].strip()
+    cook[dish] = []
+    ingredients_quantity = int(recipe[1])
+    for index in range(ingredients_quantity):
+        ingredient = recipe[index+2].split('|')
+        cook[dish].append({"ingridient_name": ingredient[0].strip(), "quantity": int(ingredient[1]), "measure": ingredient[2].strip()})
+    return cook
 
 
-def get_shop_list_by_dishes(dishes, person_count):
+def get_shop_list_by_dishes(cook_book, dishes, person_count):
     shop_list = {}
 
     for dish in dishes:
-        cook_book = {
-            'Яичница': [
-                {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
-                {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
-            ],
-            'Стейк': [
-                {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
-                {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
-                {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'}
-            ],
-            'Салат': [
-                {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
-                {'ingridient_name': 'оругцы', 'quantity': 100, 'measure': 'гр.'},
-                {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
-                {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
-            ]
-        }
-
-        dishes = ['Яичница', 'Стейк', 'Салат']
-
-        def get_shop_list_by_dishes(dishes, person_count):
-            shop_list = {}
-            for dish in dishes:
-                for ingridient in cook_book[dish]:
-                    new_shop_list_item = dict(ingridient)
-                    new_shop_list_item['quantity'] *= person_count
-                    if new_shop_list_item['ingridient_name'] not in shop_list:
-                        shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
-                    else:
-                        shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
-            return shop_list
-
-        def print_shop_list(shop_list):
-            for shop_list_item in shop_list.values():
-                print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
-
-        def create_shop_list():
-            person_count = int(input('Введите количество человек: '))
-            dishes = input('ВВедите блюдо в расчете на одного человека (через запятую): ').split(', ')
-            shop_list = get_shop_list_by_dishes(dishes, person_count)
-            print_shop_list(shop_list)
-
-        create_shop_list()
-
-        for ingridient in cook_book[dish]:
-            new_shop_list_item = dict(ingridient)
-            new_shop_list_item['quantity'] *= person_count
-            if new_shop_list_item['ingridient_name'] not in shop_list:
-                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+        for ingredient in cook_book[dish]:
+            new_shop_list_item = dict(ingredient)
+            new_shop_list_item["quantity"] *= person_count
+            if new_shop_list_item["ingridient_name"] not in shop_list:
+                shop_list[new_shop_list_item["ingridient_name"]] = new_shop_list_item
             else:
-                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+                shop_list[new_shop_list_item["ingridient_name"]]["quantity"] += new_shop_list_item["quantity"]
     return shop_list
-
 
 def print_shop_list(shop_list):
     for shop_list_item in shop_list.values():
@@ -83,18 +42,10 @@ def print_shop_list(shop_list):
 
 
 def create_shop_list():
+    cook_book = recipes()
     person_count = int(input('Введите количество человек: '))
     dishes = input('ВВедите блюдо в расчете на одного человека (через запятую): ').split(', ')
-    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    shop_list = get_shop_list_by_dishes(cook_book ,dishes, person_count)
     print_shop_list(shop_list)
 
-
-#create_shop_list()
-
-
-
-with open('new.txt', encoding= 'utf8') as f:
-    for line in f:
-        line = line.strip()
-        name = line.split(' ')
-        print(name)
+create_shop_list()
